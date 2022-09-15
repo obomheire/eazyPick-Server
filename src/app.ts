0d1ebad5334e853a3b70c7d82f45c88eeaf80723
Zack Bello <obomheire@gmail.com>
2022-09-15 02:07:37 +0100

    Setup server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log('Hello World');
+import express, {
+  Request,
+  Response,
+  NextFunction,
+  Application,
+  ErrorRequestHandler,
+} from "express";
+import { Server } from "http";
+import createHttpError from "http-errors";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { config } from "dotenv";
+config();
+
+const app: Application = express();
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req: Request, res: Response) => {
+    const products = {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+    }
+  res.send(products);
+});
+
+const PORT: number = Number(process.env.PORT) || 3000;
+
+const server: Server = app.listen(PORT, () => {
+    // console.log(api)
+    console.log(`App listning on port ${PORT}`)
+}
+);
