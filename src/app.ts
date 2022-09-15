e044083e6c685e0d1798cfbab16427d4fc1e803f
Zack Bello <obomheire@gmail.com>
2022-09-15 08:00:08 +0100

    Tested Post and Get in app.ts - working

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,30 @@ import express, {
   ErrorRequestHandler,
 } from "express";
 import { Server } from "http";
-import createHttpError from "http-errors";
+// import createHttpError from "http-errors";
 import cors from "cors";
-import cookieParser from "cookie-parser";
-import session from "express-session";
+// import cookieParser from "cookie-parser";
+// import session from "express-session";
 import { config } from "dotenv";
 config();
+import morgan from "morgan";
 
 const app: Application = express();
+
+//Middlewares
+app.use(cors());
+app.use(morgan("dev"));
+// app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(
+//   session({
+//     secret: "secretkey",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+
 const api = process.env.API_URL;
 
 app.get(`${api}/products`, (req: Request, res: Response) => {
@@ -23,6 +39,12 @@ app.get(`${api}/products`, (req: Request, res: Response) => {
         price: 100,
     }
   res.send(products);
+});
+
+app.post(`${api}/products`, (req: Request, res: Response) => {
+    const newProduct = req.body;
+    console.log(newProduct)
+  res.send(newProduct);
 });
 
 const PORT: number = Number(process.env.PORT) || 3000;
