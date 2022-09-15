35396fd6af7eaf4b3637edc839efb1f8cbd628bb
Zack Bello <obomheire@gmail.com>
2022-09-15 10:00:03 +0100

    Successfully connected to database

--- src/app.ts
+++ src/app.ts
@@ -8,27 +8,39 @@ import express, {
 import { Server } from "http";
 // import createHttpError from "http-errors";
 import cors from "cors";
-// import cookieParser from "cookie-parser";
-// import session from "express-session";
+import cookieParser from "cookie-parser";
+import session from "express-session";
 import { config } from "dotenv";
 config();
 import morgan from "morgan";
+import { connect, mongoose } from "./connection/mongoConnect";
+import { connectTestDB } from "./connection/mongoMemoryServer";
+const MongoStore = require("connect-mongodb-session")(session);
 
 const app: Application = express();
 
 //Middlewares
 app.use(cors());
 app.use(morgan("dev"));
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(
-//   session({
-//     secret: "secretkey",
-//     resave: true,
-//     saveUninitialized: true,
-//   })
-// );
+app.use(
+  session({
+    secret: "secretkey",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+if (process.env.NODE_ENV === "test") {
+  connectTestDB();
+  console.log(process.env.NODE_ENV);
+} else {
+  connect();
+  console.log(process.env.NODE_ENV);
+}
 
 const api = process.env.API_URL;
 
@@ -43,14 +55,14 @@ app.get(`${api}/products`, (req: Request, res: Response) => {
 
 app.post(`${api}/products`, (req: Request, res: Response) => {
     const newProduct = req.body;
-    console.log(newProduct)
+    console.log(newProduct);
   res.send(newProduct);
 });
 
 const PORT: number = Number(process.env.PORT) || 3000;
 
 const server: Server = app.listen(PORT, () => {
-    // console.log(api)
+    console.log(api)
     console.log(`App listning on port ${PORT}`)
 }
 );
