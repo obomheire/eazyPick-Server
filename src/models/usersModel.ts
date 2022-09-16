a79ac8e2e46e29268be33e4ae78c1917ff9ee0de
Zack Bello <obomheire@gmail.com>
2022-09-16 15:49:53 +0100

    Added usesSchema

--- src/models/usersModel.ts
+++ src/models/usersModel.ts
@@ -1,15 +1,55 @@
 import mongoose, { Schema, model, Document, Types } from "mongoose";
 
 const usersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    street: {
+        type: String,
+        default: "",
+    },
+    apartment: {
+        type: String,
+        default: "",
+    },
+    zip: {
+        type: String,
+        default: "",
+    },
+    city: {
+        type: String,
+        default: "",
+    },
+    country: {
+        type: String,
+        default: "",
+    }
   
 });
 
-// usersSchema.virtual("id").get(function () {
-//   return this._id.toHexString();
-// });
+usersSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
 
-// usersSchema.set("toJSON", {
-//   virtuals: true, 
-// });
+usersSchema.set("toJSON", {
+  virtuals: true, 
+});
 
 export const Users = model("Users", usersSchema);
